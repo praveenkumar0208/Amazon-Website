e1fe18482364c09bdb9d866b16b65bc45125d35a
praveenmdp42@gmail.com <praveenmdp42@gmail.com>
2024-10-13 08:20:01 +0530

    make the cart qutity intereactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -66,6 +66,7 @@ document.querySelectorAll('.js-add-to-cart')
 .forEach((button)=>{
   button.addEventListener('click',()=>{
   const productId=button.dataset.productId;
+
 let matchingItem;
 
 
@@ -83,8 +84,15 @@ let matchingItem;
       quantity:1
     })
   }
+  let cartQuantity=0;
 
-console.log(cart);
+  cart.forEach((item)=>{
+    cartQuantity+=item.quantity;
+  })
+
+  document.querySelector('.js-cart-quantity')
+  .innerHTML=cartQuantity;
+
 
 });
 })
